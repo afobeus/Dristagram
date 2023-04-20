ceee85a3bba9878ee5b873c199c3f567657e1038
Ksenia <kcy.2006@mail.ru>
2023-04-20 20:37:06 +0300

    posts table was created

---
+++ data/posts.py
@@ -0,0 +1,16 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Post(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'posts'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    likes = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    user = orm.relationship('User')
