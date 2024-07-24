aafebd26a9fbe1efec7db98f61cb2f0694a0510c
Artem <artem.dzichkanets@mail.ru>
2024-07-24 12:57:47 +0300

    /register rework

--- main.py
+++ main.py
@@ -34,6 +34,37 @@ def main():
     app.run(port=5000, host='127.0.0.1')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if not isinstance(current_user, mixins.AnonymousUserMixin):
+        return redirect(f"/feed")
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template("register.html", title="Регистрация",
+                                   form=form, message="Пароли не совпадают")
+        user = User()
+        user.email = form.email.data
+        user.name = form.name.data
+        user.surname = form.surname.data
+        user.nickname = form.nickname.data
+        user.birthday = form.birthday.data
+        user.hashed_password = form.password.data
+        user.set_password(form.password.data)
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title="Регистрация",
+                                   form=form, message="Такой пользователь уже есть")
+        if not form.agreement.data:
+            return render_template("register.html", title="Регистрация",
+                                   form=form, message="Соласитесь с политикой")
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')  # Todo: add a message "User is registered successfully"
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @app.route("/view_comments/<int:post_id>")
 def view_comments(post_id):
     db_sess = db_session.create_session()
@@ -154,38 +185,6 @@ def logout():
         return redirect("/login")
     logout_user()
     return redirect("/")
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if not isinstance(current_user, mixins.AnonymousUserMixin):
-        return redirect(f"/user/{current_user.nickname}")
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template("register.html", title="Регистрация",
-                                   form=form, message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title="Регистрация",
-                                   form=form, message="Такой пользователь уже есть")
-        if not form.agreement.data:
-            return render_template("register.html", title="Регистрация",
-                                   form=form, message="Соласитесь с политикой")
-
-        user = User(
-            email=form.email.data,
-            name=form.name.data,
-            surname=form.surname.data,
-            nickname=form.nickname.data,
-            birthday=form.birthday.data,
-            hashed_password=form.password.data,
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/user/<nickname>')
